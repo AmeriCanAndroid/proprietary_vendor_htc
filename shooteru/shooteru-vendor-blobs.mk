35f2bfe95fe5dc833e4f83952e10b825859054a8
Fraser <fraser@Media-VirtualBox.(none)>
2013-02-01 12:59:00 -0700

    add firmwares to proprietary folder

--- shooteru/shooteru-vendor-blobs.mk
+++ shooteru/shooteru-vendor-blobs.mk
@@ -19,7 +19,8 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shooteru/proprietary/camera.default.so:obj/lib/hw/camera.default.so \
     vendor/htc/shooteru/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so \
     vendor/htc/shooteru/proprietary/libacdbloader.so:obj/lib/libacdbloader.so \
-    vendor/htc/shooteru/proprietary/libacdbmapper.so:obj/lib/libacdbmapper.so
+    vendor/htc/shooteru/proprietary/libacdbmapper.so:obj/lib/libacdbmapper.so \
+    vendor/htc/shooteru/proprietary/libv8.so:obj/lib/libv8.so
 
 # All the blobs necessary for shooteru
 PRODUCT_COPY_FILES += \
@@ -101,6 +102,29 @@ PRODUCT_COPY_FILES +=  \
     vendor/htc/shooteru/proprietary/libwvm.so:system/vendor/lib/libwvm.so \
     vendor/htc/shooteru/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
     vendor/htc/shooteru/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
+
+# Firmwares
+PRODUCT_COPY_FILES += \
+    vendor/htc/shooteru/proprietary/firmware/modem.b00:/system/vendor/firmware/modem.b00 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b01:/system/vendor/firmware/modem.b01 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b02:/system/vendor/firmware/modem.b02 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b03:/system/vendor/firmware/modem.b03 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b04:/system/vendor/firmware/modem.b04 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b05:/system/vendor/firmware/modem.b05 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b06:/system/vendor/firmware/modem.b06 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b07:/system/vendor/firmware/modem.b07 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b08:/system/vendor/firmware/modem.b08 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b09:/system/vendor/firmware/modem.b09 \
+    vendor/htc/shooteru/proprietary/firmware/modem.mdt:/system/vendor/firmware/modem.mdt \
+    vendor/htc/shooteru/proprietary/firmware/q6.b00:/system/vendor/firmware/q6.b00 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b01:/system/vendor/firmware/q6.b01 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b02:/system/vendor/firmware/q6.b02 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b03:/system/vendor/firmware/q6.b03 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b04:/system/vendor/firmware/q6.b04 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b05:/system/vendor/firmware/q6.b05 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b06:/system/vendor/firmware/q6.b06 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b07:/system/vendor/firmware/q6.b07 \
+    vendor/htc/shooteru/proprietary/firmware/q6.mdt:/system/vendor/firmware/q6.mdt
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
